7eebd56fbe2b583fdf529c0bba5ab47d767ce5f3
Dante <42275387+TJANGEL@users.noreply.github.com>
2019-06-23 16:41:12 -0400

    fixed error with return function syntax

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -2,16 +2,16 @@ import React from 'react';
 
 const Posts = ({ posts, loading }) => {
   if (loading) {
-    return <h2>Loading</h2>;
+    return <h2>Loading...</h2>;
   }
 
   return (
     <ul className='list-group mb-4'>
-      {posts.map(post => {
+      {posts.map(post => (
         <li key={post.id} className='list-group-item'>
           {post.title}
-        </li>;
-      })}
+        </li>
+      ))}
     </ul>
   );
 };
